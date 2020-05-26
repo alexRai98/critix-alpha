0084226632df5c55f383690448d2576700cfaa6e
Rai <raikcuerda19@gmail.com>
2020-05-26 01:19:26 -0500

    change column anme

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_034642) do
+ActiveRecord::Schema.define(version: 2020_05_26_060339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,7 +70,7 @@ ActiveRecord::Schema.define(version: 2020_05_26_034642) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "unsername"
+    t.string "username"
     t.string "email"
     t.date "brith_date"
     t.integer "review_count"
