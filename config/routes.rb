42a3c51513b45d144efd1a026b4388e489f40001
Rai <raikcuerda19@gmail.com>
2020-05-28 01:28:17 -0500

    devise for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   
+  get 'users/show'
+  root to: 'home#index'
+  devise_for :users
+  #resources :users, only: :show
+  get '/my_profile', to: 'users#show'
   resources :companies, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
